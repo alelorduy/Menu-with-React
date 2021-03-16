7799c67c25b98edb9def0af6b4cbbc3e0cb77397
alelorduym <alelorduym@gmail.com>
2021-03-15 21:40:18 -0300

    Add navbar component

---
+++ src/components/ListItem.js
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+
+const ListItem = (props) => {
+  const [isActive, setIsActive] = useState(false);
+
+  return (
+    <li style={{ position: "relative" }}>
+      <a
+        onMouseEnter={() => setIsActive(true)}
+        onMouseLeave={() => setIsActive(false)}
+        className={isActive && "active"}
+        href="/"
+      >
+        {props.text}
+      </a>
+      <div className={isActive ? "animation" : ""}> </div>
+    </li>
+  );
+};
+export default ListItem;
